5c0852031848ba442dab4c9be82a3b7d7652fc80
Theprogrammingmachine <62161534+Theprogrammingmachine@users.noreply.github.com>
2020-04-20 00:29:18 +0100

    Update printTable.py

--- printTable.py
+++ printTable.py
@@ -1,35 +1,31 @@
-#!/usr/bin/python3
 
-tableData = [
-    ['apples', 'oranges', 'cherries', 'banana'],
-    ['Alice', 'Bob', 'Carol', 'David'],
-    ['dogs', 'cats', 'moose', 'goose'],
-    ]
+tableData = [["apples", "oranges", "cherries", "banana"],
+             ["Alice", "Bob", "Carol", "David"],
+             ["dogs", "cats", "moose", "goose"]]
 
-def colWidth(tableData):
-    colWidths = [0] * len(tableData)
-    for col in range(len(tableData)):
-        for item in tableData[col]:
-            if len(item) > colWidths[col]:
-                colWidths[col] = len(item)
-    return colWidths
+def prinTable(listoflist):
+    colWidth = [0] * len(listoflist)
+    n = 0
+    while n < len(listoflist):
+        maxLen = 0
+        for element in listoflist[n]:
+            if len(element) > maxLen:
+                maxLen = len(element)
+        colWidth[n] = maxLen
+        n = n + 1
+    newList = []
+    i = 0
+    while i < len(listoflist[0]):
+        j = 0
+        concave = []
+        while j < (len(listoflist)):
+            concave.append(listoflist[j][i].rjust(colWidth[j]))
+            j = j + 1
+        newList.append(concave)
+        print(" ".join(newList[i]))
+        i = i + 1
 
-def pivot(tableData):
-    columns = []
-    for item in range(len(tableData[0])):
-        for col in range(len(tableData)):
-            try:
-                columns[item].append(tableData[col][item])
-            except:
-                columns.append([tableData[col][item]])
-    return columns
+prinTable(tableData)
 
-def print_table(tableData):
-    length = colWidth(tableData)
-    for row in pivot(tableData):
-        line = ''
-        for item in range(len(row)):
-            line += row[item].rjust(length[item] + 1)
-        print line
 
-print_table(tableData)
+#my take on the subject 
